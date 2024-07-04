a4ae01bcafec2a45d21292be138fd326f392648c
KarthikaMarudhachalam <karthika16704@gmail.com>
2024-07-04 15:28:01 +0530

    Add files

---
+++ Games/game.java
@@ -0,0 +1,33 @@
+public class game
+{
+    public void fizzbuss(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0 && i%5==0)
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if(i%3==0)
+            {
+                System.out.println("Fizz");
+            }
+            else if(i%5==0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }
+    }
+
+
+public static void main(String[] args) 
+{
+    game result=new game();
+
+    result.fizzbuss(10);
+}
+}
