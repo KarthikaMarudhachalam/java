d1b7e5d228555d1e872bc6382b37e9cfb9a11d77
KarthikaMarudhachalam <162821625+KarthikaMarudhachalam@users.noreply.github.com>
2024-07-04 20:28:04 +0530

    Add files

---
+++ arrrr.java
@@ -0,0 +1,20 @@
+public class arrrr {
+  public static void main(String[] args) {
+    int num[]=new int[5];
+    num[0]=1;
+    num[1]=2;
+    num[2]=3;
+    num[3]=4;
+    num[4]=5;
+
+    // for(int i=0;i<num.length;i++)
+    // {
+    //   System.out.println(num[i]);
+    // }
+
+    for(int n : num)
+    {
+      System.out.println(n);
+    }
+  }
+}
